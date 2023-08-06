aaaa08bce1f77232b1fd2391d7c3afb325f88b2e
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-06 10:49:11 +0000

    practice project completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,15 @@
-// Write your JS code here
+import LogoutButton from '../LogoutButton'
+
+import Header from '../Header'
+
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <Header />
+    <h1>About Route</h1>
+    <LogoutButton />
+  </div>
+)
+
+export default About
